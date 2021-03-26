05c7143845ecbc6cde7b928f8de204729704f011
MCBaileyGitHub <54681949+MCBaileyGitHub@users.noreply.github.com>
2021-03-26 13:00:00 -0400

    updated

--- Alogrithm.py
+++ Alogrithm.py
@@ -1,10 +1,10 @@
-def search_block(proc_dic, size, memory) -> bool:
-    start_index = first_fit.index(0, 0, MEMORY_SIZE - 1)
+def search_block(proc_dict, size, memory) -> bool:
+    start_index = memory.index(0, 0, MEMORY_SIZE - 1)
 
     current_size = 1
 
     for i in range(start_index + size):
-        if first_fit[i] == 1:
+        if memory[i] == 1:
             continue
         if current_size == size:
             break
@@ -13,84 +13,17 @@ def search_block(proc_dic, size, memory) -> bool:
     end_index = start_index + current_size
 
     for i in range(start_index, end_index):
-        first_fit[i] = 1
+        memory[i] = 1
 
     proc_dict[start_index] = size
 
 
-MEMORY_SIZE = 10
-
-proc_dict = {}
-
-first_fit = [0] * MEMORY_SIZE
-
-print(first_fit)
-#############################################
-size = 3
-
-start_index = first_fit.index(0, 0, MEMORY_SIZE - 1)
-
-current_size = 1
-
-for i in range(start_index + size):
-    if first_fit[i] == 1:
-        continue
-    if current_size == size:
-        break
-    current_size += 1
-
-end_index = start_index + current_size
-
-for i in range(start_index, end_index):
-    first_fit[i] = 1
-
-proc_dict[start_index] = size
-#############################################
-
-size = 4
-
-start_index = first_fit.index(0, 0, MEMORY_SIZE - 1)
-
-current_size = 1
-
-for i in range(start_index + size):
-    if first_fit[i] == 1:
-        continue
-    if current_size == size:
-        break
-    current_size += 1
-
-end_index = start_index + current_size
-
-for i in range(start_index, end_index):
-    first_fit[i] = 1
-
-proc_dict[start_index] = size
-################################################
-size = 3
-
-start_index = first_fit.index(0, 0, MEMORY_SIZE - 1)
-
-current_size = 1
-
-for i in range(start_index + size):
-    if first_fit[i] == 1:
-        continue
-    if current_size == size:
-        break
-    current_size += 1
-
-end_index = start_index + current_size
-
-for i in range(start_index, end_index):
-    first_fit[i] = 1
-
-proc_dict[start_index] = size
-
-
-print(first_fit)
-
-print(proc_dict)
-
-
 if __name__ == '__main__':
+    MEMORY_SIZE = 10
+    proc_dict = {}
+    first_fit = [0] * MEMORY_SIZE
+    sizes = [3, 4, 3]
+
+    for size in sizes:
+        search_block(proc_dict, size, first_fit)
+        print(first_fit)
