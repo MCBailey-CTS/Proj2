5f91fb5378521710fd06df026503c70c4c525375
MCBaileyGitHub <54681949+MCBaileyGitHub@users.noreply.github.com>
2021-04-16 09:55:02 -0400

    updated

--- Alogrithm.py
+++ Alogrithm.py
@@ -1,3 +1,8 @@
+from multiprocessing import Process
+from threading import Timer
+from time import sleep
+
+
 def search_block(proc_dict, size, memory) -> bool:
     start_index = memory.index(0, 0, MEMORY_SIZE - 1)
 
@@ -18,11 +23,26 @@ def search_block(proc_dict, size, memory) -> bool:
     proc_dict[start_index] = size
 
 
+def new_process(memory, size, run_time, proc_dict, start_mem):
+    sleep(run_time)
+    for i in range()
+
+
 if __name__ == '__main__':
+    timer = Timer()
+    timer.start()
     MEMORY_SIZE = 10
     proc_dict = {}
     first_fit = [0] * MEMORY_SIZE
-    sizes = [3, 4, 3]
+    sizes = [3, 4, 3, 1, 1, 5, 6, 7]
+    times = [1, 1, 1, 1, 1, 1, 1, 1]
+
+    processes = []
+
+    for index in range(len(sizes)):
+        process = Process(target=new_process, args=(
+            first_fit, sizes[index], times[index], proc_dict, start_mem))
+        processes.append(process)
 
     for size in sizes:
         search_block(proc_dict, size, first_fit)
